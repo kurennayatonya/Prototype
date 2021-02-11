4b0061c25f69b41c7594ec60dbc75c2e11342cd9
kurennayatonya <kurennayatonya@gmail.com>
2021-02-11 20:01:16 +0300

    Upload to Git

---
+++ Prototype/Assets/Scripts/DragAndDropController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropController : MonoBehaviour
+{
+    public bool isDraggable = true;
+    public bool isDragged = false;
+    void Update()
+    {
+        if(isDragged)
+            transform.position = Input.mousePosition;
+    }
+    
+    private void OnMouseOver()
+    {
+        if (isDraggable && Input.GetMouseButtonDown(0))
+        {
+            isDragged = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
